a058446e0053426d496838366bb264b2378fa5bb
Felipe <fe.ferradap@duocuc.cl>
2024-07-03 23:40:11 -0400

    Cambios de estilo al carrito y otros

--- autos_del_mar/urls.py
+++ autos_del_mar/urls.py
@@ -17,9 +17,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from concesionario import views as concesionario_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', concesionario_views.landing, name='landing'),
     path('concesionario/', include('concesionario.urls')),
     path('autenticacion/', include('autenticacion.urls')),
     path('repuestos/', include('repuestos.urls')),
--- concesionario/urls.py
+++ concesionario/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/home',views.home, name='home'),
     path('', views.landing, name='landing'),
     path('vehiculos/', views.lista_vehiculos, name='lista_vehiculos'),
     path('cotizar/<str:categoria>/<int:vehiculo_id>/', views.solicitar_cotizacion, name='solicitar_cotizacion'),
